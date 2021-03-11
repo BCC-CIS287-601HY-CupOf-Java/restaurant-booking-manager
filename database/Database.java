e804fa314c36e337bc5a68e2bd117fb04464d7ad
Dan Habot <dan.habot@gmail.com>
2021-03-11 01:43:30 -0500

    fixed missing files

--- database/Database.java
+++ database/Database.java
@@ -10,8 +10,8 @@ public abstract class Database<Type> {
 
   private static final String DELIMITER = "~";
 
-  public Database(String collection) throws FileNotFoundException {
-    this.path = collection;
+  public Database(String collection) {
+    this.path = collection + ".db";
   }
 
   protected Type deserialize(String[] data) throws NotImplementedException {
@@ -22,37 +22,43 @@ public abstract class Database<Type> {
     throw new NotImplementedException("Requires serialize function");
   }
 
-  public final ArrayList<Type> get() throws IOException, NotImplementedException {
-    BufferedReader reader = new BufferedReader(new FileReader(this.path));
-    ArrayList<Type> list = new ArrayList<>();
-    String line;
+  protected final ArrayList<Type> get() throws IOException, NotImplementedException {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(this.path));
+      ArrayList<Type> list = new ArrayList<>();
+      String line = reader.readLine();
 
-    do {
-      line = reader.readLine();
-      String[] items = line.split(Database.DELIMITER);
+      while (line != null) {
+        String[] items = line.split(Database.DELIMITER);
+        list.add(this.deserialize(items));
+        line = reader.readLine();
+      }
+      reader.close();
 
-      list.add(this.deserialize(items));
-    } while (line != null);
-    reader.close();
-    
-    return list;
+      return list;
+    } catch (FileNotFoundException e) {
+      File f = new File(this.path);
+      f.createNewFile();
+      return this.get();
+    }
   }
 
-  public void set(ArrayList<Type> data) throws IOException, NotImplementedException {
-    FileWriter writer = new FileWriter(this.path, false);
+  protected void set(ArrayList<Type> data) throws IOException, NotImplementedException {
+    try {
+      FileWriter writer = new FileWriter(this.path, false);
 
-    for (Type item : data) {
-      String[] items = this.serialize(item);
-      
-      for (Integer i = 0; i < items.length; i++) {
-        items[i] = "\"" + items[i] + "\"";
+      for (Type item : data) {
+        String[] items = this.serialize(item);
+        String line = String.join(Database.DELIMITER, items);
+        writer.append(line + "\n");
       }
 
-      String line = String.join(Database.DELIMITER, items);
-      writer.append(line);
+      writer.flush();
+      writer.close();
+    } catch (FileNotFoundException e) {
+      File f = new File(this.path);
+      f.createNewFile();
+      this.set(data);
     }
-
-    writer.flush();
-    writer.close();
   }
 }
