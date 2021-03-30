887ce9f0d972dbea4604112cd1d64854ebd29ad1
OmniaXP <mcfplayer@gmail.com>
2021-03-29 22:43:38 -0400

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    JFrame frame = new JFrame("Current Reservations");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+	    frame.setSize(600,600);
+	    frame.setResizable(false);
+	    frame.getContentPane().setBackground(Color.gray);
+
+	    Jlist<> list = new Jlist<>;
+
+
+    }
+}
